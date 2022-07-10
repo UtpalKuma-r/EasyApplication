f2f33e3448b837a699760bcd4dc13b35b1d6cd05
gauravpg <109019613+gauravpg@users.noreply.github.com>
2022-07-10 14:15:44 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,4 +1,29 @@
 <!DOCTYPE html>
+<html>
+<body bgcolor="lightyellow">
+<FORM><fieldset><legend><ALIGN="CENTER">LOGIN</ALIGN="CENTER"></legend>
+		<TABLE ALIGN="CENTER">
+			<TR>
+				<TD WIDTH="200">User Name:</TD>
+				<TD WIDTH="600"><INPUT TYPE="TEXT" NAME="UserName" required></TD>
+				<TD WIDTH="100"></TD>
+			</tr>
+			<TR>
+				<TD>Password</TD>
+				<TD><INPUT TYPE="password" INPUT="password" required></TD>
+				<TD></TD>
+			</tr>
+                        <TR>
+			
+				<TD ><button type="submit">Login</button></TD>
+				<TD><INPUT TYPE="RESET"  VALUE="RESET">&nbsp;&nbsp;&nbsp;</TD>
+			</tr>
+                        
+
+
+</form>
+
+</body>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
